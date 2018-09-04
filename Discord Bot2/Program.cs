f306589d8a9564e8f314c24a51e98033a501d264
Jacob MURRAY <MUR0039@hcsc.vic.edu.au>
2018-09-04 15:29:37 +1000

    first commit

---
+++ Discord Bot2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Discord_Bot2
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
